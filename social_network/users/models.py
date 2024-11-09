de4c40428ee3bf3e98da924c097f6674bd8c6318
abrigo132 <garipotter97@gmail.com>
2024-11-09 14:55:53 +0500

    add is_active, is_staff, username filed, required fields

--- social_network/users/models.py
+++ social_network/users/models.py
@@ -1,8 +1,14 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
 
-class Users(models.Model):
-    username = models.CharField(max_length=20)
+class Users(AbstractBaseUser):
+    username = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=40)
-    e_mail = models.CharField(max_length=20, blank=True)
-    wallet_address = models.CharField(blank=True)
+    e_mail = models.CharField(max_length=20, blank=True, unique=True)
+    wallet_address = models.CharField(blank=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["password"]

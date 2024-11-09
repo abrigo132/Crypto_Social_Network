8f2a0eb29426608d25a2279a159939f1b53aead9
abrigo132 <garipotter97@gmail.com>
2024-11-09 14:26:26 +0500

    create user model

--- social_network/users/models.py
+++ social_network/users/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 
-class Users: ...
+class Users(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=40)
+    e_mail = models.CharField(max_length=20, blank=True)
+    wallet_address = models.CharField(blank=True)

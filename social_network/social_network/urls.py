b43244d3f4a01d5ba0d6f7aa8f2c1eab5e0072c9
abrigo132 <garipotter97@gmail.com>
2024-11-12 20:41:39 +0500

    add posts url

--- social_network/social_network/urls.py
+++ social_network/social_network/urls.py
@@ -22,5 +22,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/", include("users.urls")),
+    path("posts/", include("posts.urls")),
     path("", TemplateView.as_view(template_name="index.html"), name="home"),
 ]

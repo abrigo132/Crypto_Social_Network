0a3ed1df6910ac0b8af54c5c70df701b1abb6377
abrigo132 <garipotter97@gmail.com>
2024-11-12 20:41:59 +0500

    create view for create post

--- social_network/posts/views.py
+++ social_network/posts/views.py
@@ -1,3 +1,21 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
+from requests import Response
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from posts.models import Posts
+
+
+class CreatePost(APIView):
+    def post(
+        self,
+        request,
+    ):
+        queryset = Posts.objects.create(
+            title=request.data["title"], body=request.data["body"]
+        )
+        return Response({"message": model_to_dict(queryset)})
+
+    def get(self, request):
+        return Response({"message": "Метод GET не разрешён"})

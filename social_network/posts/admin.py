475383a01ccd13f726f406ead36e964054690efa
abrigo132 <garipotter97@gmail.com>
2024-11-12 19:47:09 +0500

    register posts model

--- social_network/posts/admin.py
+++ social_network/posts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Posts
+
+
+@admin.register(Posts)
+class PostsAdmin(admin.ModelAdmin):
+    search_fields = [
+        "title",
+        "body",
+        "created_at",
+        "like_count",
+        "dislike_count",
+        "reposts_count",
+    ]

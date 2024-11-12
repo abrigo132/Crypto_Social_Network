aaf18ea97c095556c152e55e684da05767f3479e
abrigo132 <garipotter97@gmail.com>
2024-11-12 19:44:15 +0500

    create posts model

--- social_network/posts/models.py
+++ social_network/posts/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 
 
-
 class Posts(models.Model):
     title = models.CharField(max_length=20)
     body = models.CharField(max_length=5000)
